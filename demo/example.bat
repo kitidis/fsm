c0ab04b478c71a3e816aa94292957b53bd998d66
AKolomeets <anton.kolomeets@neulion.com>
2018-10-19 17:14:20 +0700

    -- added -- added new checks

--- demo/example.bat
+++ demo/example.bat
@@ -3,14 +3,16 @@
 ..\test.exe -i etalon.fsm -o 2.seq -m black_box -mt hsi
 
 ..\test.exe -i nd.fsm -o 3.seq -m white_box
+..\test.exe -i not_fully_defined.fsm -o 4.seq -m white_box
 
-..\test.exe -i nd.fsm -o 4.seq -m black_box -mt transition_tour
-..\test.exe -i not_connected.fsm -o 5.seq -m black_box -mt transition_tour
+..\test.exe -i nd.fsm -o 5.seq -m black_box -mt transition_tour
+..\test.exe -i not_connected.fsm -o 6.seq -m black_box -mt transition_tour
+..\test.exe -i not_fully_defined.fsm -o 7.seq -m black_box -mt transition_tour
 
-..\test.exe -i nd.fsm -o 6.seq -m black_box -mt hsi
-..\test.exe -i not_connected.fsm -o 7.seq -m black_box -mt hsi
-..\test.exe -i not_fully_defined.fsm  -o 8.seq -m black_box -mt hsi
+..\test.exe -i nd.fsm -o 8.seq -m black_box -mt hsi
+..\test.exe -i not_connected.fsm -o 9.seq -m black_box -mt hsi
+..\test.exe -i not_fully_defined.fsm  -o 10.seq -m black_box -mt hsi
 
-..\test.exe -i lit.fsm -o 9.seq -m white_box
-..\test.exe -i lit.fsm -o 10.seq -m black_box -mt transition_tour
-..\test.exe -i lit.fsm -o 11.seq -m black_box -mt hsi
+..\test.exe -i lit.fsm -o 11.seq -m white_box
+..\test.exe -i lit.fsm -o 12.seq -m black_box -mt transition_tour
+..\test.exe -i lit.fsm -o 13.seq -m black_box -mt hsi
